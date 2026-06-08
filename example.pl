dbb7847a3a1ed7f410e6196cf13df44a518456e2
Scott Baker <scott@perturb.org>
2026-06-08 14:18:43 -0700

    Update example

--- example.pl
+++ example.pl
@@ -20,18 +20,7 @@ printf "Dice roll: %d\n", $roll;
 
 # --- Generate normally-distributed numbers (Box-Muller) ---
 my @samples;
-while (@samples < 6) {
-    my $u1 = $rng->next_double;
-    my $u2 = $rng->next_double;
-    next if $u1 == 0;
-    my $r = sqrt(-2 * log($u1));
-    push @samples, sprintf '%.4f', $r * cos(6.28318530717959 * $u2);
-    push @samples, sprintf '%.4f', $r * sin(6.28318530717959 * $u2);
+for (1 .. 5) {
+    push(@samples, $rng->next_double());
 }
-printf "Normal samples: %s\n", join ', ', @samples[0..5];
-
-# --- Parallel stream demo ---
-my $base_seed = 12345;
-my @streams = map { Math::Random::Biski64->for_stream($base_seed, $_, 4) } 0..3;
-printf "Parallel stream first values:\n";
-printf "  Stream %d: %u\n", $_, $streams[$_]->next_u64 for 0..3;
+printf("Normal samples: %s\n", join ', ', @samples);
